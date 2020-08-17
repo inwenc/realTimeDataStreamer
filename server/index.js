7124c361a3f3586d9feb3dfc438c3a6cf83397aa
Yingwen <inwen.chen@gmail.com>
2020-08-17 08:12:11 -0600

    setup boilerplate

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+//const bodyParser = require('body-parser');
+const path = require('path');
+
+const io = require('socket.io')(3000);
+
+const needle = require('needle');
+const TWITTER_TOKEN = require('./config.js')
+
+
+
+const streamURL = 'https://api.twitter.com/2/tweets/sample/stream';
+//app.get('/', (req, res) => {
+io.on('connect', (socket) => {
+  console.log('a user connected')
+  const options = {
+    timeout: 2000
+  }
+
+ var stream = needle.get(streamURL, {
+    headers: {
+      Authorization: `Bearer ${TWITTER_TOKEN}`
+    }
+  }, options)
+  stream.on('data', data => {
+    try {
+      //incoming data is buffer
+      const json = JSON.parse(data);
+      //res.json(json)
+      socket.emit('twitter', json)
+    } catch (e) {
+      // Keep alive signal received. Do nothing.
+    }
+  }).on('error', error => {
+    if (error.code === 'ETIMEDOUT') {
+      stream.emit('timeout');
+    }
+  });
+
+
+
+})
