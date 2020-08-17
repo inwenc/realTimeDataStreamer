a2fbaa7d15986175805029482547af08538a629e
Yingwen <inwen.chen@gmail.com>
2020-08-17 14:06:46 -0600

    data streaming on client side

--- client/src/app/real-time-data/real-time-data.component.ts
+++ client/src/app/real-time-data/real-time-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WebSocketService } from '../web-socket.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-real-time-data',
@@ -9,12 +10,25 @@ import { WebSocketService } from '../web-socket.service';
 export class RealTimeDataComponent implements OnInit {
 
   title = 'client';
+
+  //$messages = [];
+  public storage: Array<Object> = [];
+  //public tweet: any;
   constructor(private webSocketService: WebSocketService) {
 
   }
   ngOnInit() {
-    this.webSocketService.listen('twitter').subscribe((data) => {
-      console.log(data);
+    this.webSocketService.listen('twitter').subscribe((data:any) => {
+     // let newData = JSON.parse(data);
+     // console.log('newData', data)
+    //  let newData = { data }
+     //this.tweet = data;
+
+    this.storage.push(data)
+    // if (this.storage.length !== 0) {
+    //   this.storage = this.storage.shift()
+    // }
+    console.log(this.storage)
     })
   }
 
